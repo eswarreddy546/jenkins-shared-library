de293ef143bdd4735dabeeb451073a7ab65e16de
Asri <asri>
2026-08-01 22:45:25 +0530

    Initial commit

---
+++ jenkins-terraform-eks/70-acm/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "domain_name" {
+    default = "eswar.fun"
+}
+
+variable "zone_id" {
+    default = "Z0883794XJ2EA3764V8N"
+}
---
+++ jenkins-terraform-eks/90-eks/variables.tf
@@ -0,0 +1,35 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z0883794XJ2EA3764V8N"
+}
+
+variable "domain_name" {
+    default = "eswar.fun"
+}
+
+variable "eks_version" {
+
+}
+
+variable "eks_nodegroup_blue_version" {
+
+}
+
+variable "eks_nodegroup_green_version" {
+
+}
+
+variable enable_blue {
+
+}
+
+variable enable_green {
+    
+}
