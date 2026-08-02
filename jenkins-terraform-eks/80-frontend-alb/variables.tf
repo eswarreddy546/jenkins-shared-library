74096629d90bd8854a89ccf3792a16558fad5a8f
Asri <asri>
2026-08-02 14:03:54 +0530

    Updated shared library

--- jenkins-terraform-eks/80-frontend-alb/variables.tf
+++ jenkins-terraform-eks/80-frontend-alb/variables.tf
@@ -7,9 +7,9 @@ variable "environment" {
 }
 
 variable "zone_id" {
-    default = "Z0948150OFPSYTNVYZOY"
+    default = "Z0883794XJ2EA3764V8N"
 }
 
 variable "domain_name" {
-    default = "daws86s.fun"
+    default = "eswar.fun"
 }
