63b8595c9d710cca5ba99d30a41cc3a532dc20f2
Asri <asri>
2026-08-03 08:11:27 +0530

    shared library

---
+++ vars/nodejsshared.groovy
@@ -0,0 +1,316 @@
+def call (Map configMap){
+
+pipeline {
+
+    agent {
+        node {
+            label 'AGENT-1'
+        }
+    }
+
+    environment {
+        COURSE      = "Jenkins"
+        appVersion  = ""
+        ACC_ID      = "526426842890"
+        PROJECT   = configMap.get("project")
+        COMPONENT = configMap.get("component")
+        REGION      = "us-east-1"
+
+        ECR_REPO    = "${ACC_ID}.dkr.ecr.${REGION}.amazonaws.com/${PROJECT}/${COMPONENT}"
+    }
+
+
+    options {
+        timeout(time: 30, unit: 'MINUTES')
+        disableConcurrentBuilds()
+    }
+
+    stages {
+
+        stage('Checkout') {
+            steps {
+                checkout scm
+            }
+        }
+
+        stage('Read Version') {
+            steps {
+                dir('jenkins-cicd') {
+                    script {
+                        def packageJSON = readJSON file: 'package.json'
+                        appVersion = packageJSON.version
+                        echo "Application Version : ${appVersion}"
+                    }
+                }
+            }
+        }
+
+        // stage('Install Dependencies') {
+        //     steps {
+        //         sh '''
+        //             npm install
+        //         '''
+        //     }
+        // }
+
+        stage('Install Dependencies') {
+            steps {
+                dir('jenkins-cicd') {
+                    sh 'npm install'
+                }
+            }
+        }
+
+        stage('SonarQube Scan') {
+            steps {
+                script {
+
+                    def scannerHome = tool 'SonarScanner'
+
+                    withSonarQubeEnv('sonar-qube') {
+                        sh """
+                            ${scannerHome}/bin/sonar-scanner \
+                              -Dsonar.projectKey=${COMPONENT} \
+                              -Dsonar.projectName=${COMPONENT} \
+                              -Dsonar.sources=. \
+                              -Dsonar.projectVersion=${appVersion}
+                        """
+                    }
+                }
+            }
+        }
+
+        stage('Quality Gate') {
+            steps {
+                timeout(time: 1, unit: 'HOURS') {
+                    waitForQualityGate abortPipeline: false
+                }
+            }
+        }
+
+        stage('Docker Build') {
+            steps {
+              dir('jenkins-cicd') {
+                 sh """
+                    docker build -t ${PROJECT}/${COMPONENT}:${appVersion} .
+                 """
+                }
+            }
+        }
+        /*
+        stage('Trivy Image Scan') {
+            steps {
+                sh """
+                    echo "=========================================="
+                    echo "      Trivy Image Vulnerability Scan"
+                    echo "=========================================="
+
+                    docker images
+
+                    trivy image \
+                      --scanners vuln \
+                      --severity LOW,MEDIUM,HIGH,CRITICAL \
+                      --format table \
+                      --ignore-unfixed \
+                      localhost/${PROJECT}/${COMPONENT}:${appVersion}
+                """
+            }
+        }
+        */
+
+        stage('Login to AWS ECR') {
+            steps {
+                withCredentials([[
+                    $class: 'AmazonWebServicesCredentialsBinding',
+                    credentialsId: 'aws-cred'
+                ]]) {
+
+                    sh """
+                        aws ecr get-login-password --region ${REGION} | \
+                        docker login \
+                          --username AWS \
+                          --password-stdin \
+                          ${ACC_ID}.dkr.ecr.${REGION}.amazonaws.com
+                    """
+                }
+            }
+        }
+
+        stage('Tag Docker Image') {
+            steps {
+                sh """
+                    docker tag ${PROJECT}/${COMPONENT}:${appVersion} ${ECR_REPO}:${appVersion}
+                    docker tag ${PROJECT}/${COMPONENT}:${appVersion} ${ECR_REPO}:latest
+                """
+            }
+        }
+
+        stage('Push Image to ECR') {
+            steps {
+                sh """
+                    docker push ${ECR_REPO}:${appVersion}
+                    docker push ${ECR_REPO}:latest
+                """
+            }
+        }
+
+        // stage('Terraform VPC') {
+        //     steps {
+        //         dir('/home/ec2-user/jenkins-shared-library/jenkins-terraform-eks/00-vpc') {
+
+        //             withCredentials([[
+        //                 $class: 'AmazonWebServicesCredentialsBinding',
+        //                 credentialsId: 'aws-cred'
+        //             ]]) {
+
+        //                 sh '''
+        //                     terraform init
+        //                     terraform validate
+        //                     terraform plan
+        //                     terraform apply -auto-approve
+        //                 '''
+        //             }
+        //         }
+        //     }
+        // }
+
+        // stage('Terraform Security Groups') {
+        //     steps {
+        //         dir('/home/ec2-user/jenkins-shared-library/jenkins-terraform-eks/10-sg') {
+
+        //             withCredentials([[
+        //                 $class: 'AmazonWebServicesCredentialsBinding',
+        //                 credentialsId: 'aws-cred'
+        //             ]]) {
+
+        //                 sh '''
+        //                     terraform init
+        //                     terraform validate
+        //                     terraform plan
+        //                     terraform apply -auto-approve
+        //                 '''
+        //             }
+        //         }
+        //     }
+        // }
+
+        // stage('Terraform Bastion') {
+        //     steps {
+        //         dir('/home/ec2-user/jenkins-shared-library/jenkins-terraform-eks/20-bastion') {
+        //             withCredentials([[
+        //                 $class: 'AmazonWebServicesCredentialsBinding',
+        //                 credentialsId: 'aws-cred'
+        //             ]]) {
+
+        //                 sh '''
+        //                     terraform init
+        //                     terraform validate
+        //                     terraform plan
+        //                     terraform apply -auto-approve
+        //                 '''
+        //             }
+        //         }
+        //     }
+        // }
+
+        // stage('Terraform SG Rules') {
+        //     steps {
+        //         dir('/home/ec2-user/jenkins-shared-library/jenkins-terraform-eks/30-sg-rules') {
+        //             withCredentials([[
+        //                 $class: 'AmazonWebServicesCredentialsBinding',
+        //                 credentialsId: 'aws-cred'
+        //             ]]) {
+
+        //                 sh '''
+        //                     terraform init
+        //                     terraform validate
+        //                     terraform plan
+        //                     terraform apply -auto-approve
+        //                 '''
+        //             }
+        //         }
+        //     }
+        // }
+
+        // stage('Terraform ACM') {
+        //     steps {
+        //         dir('/home/ec2-user/jenkins-shared-library/jenkins-terraform-eks/70-acm') {
+        //             withCredentials([[
+        //                 $class: 'AmazonWebServicesCredentialsBinding',
+        //                 credentialsId: 'aws-cred'
+        //             ]]) {
+
+        //                 sh '''
+        //                     terraform init
+        //                     terraform validate
+        //                     terraform plan
+        //                     terraform apply -auto-approve
+        //                 '''
+        //             }
+        //         }
+        //     }
+        // }
+
+        // stage('Terraform Frontend ALB') {
+        //     steps {
+        //         dir('/home/ec2-user/jenkins-shared-library/jenkins-terraform-eks/80-frontend-alb') {
+        //             withCredentials([[
+        //                 $class: 'AmazonWebServicesCredentialsBinding',
+        //                 credentialsId: 'aws-cred'
+        //             ]]) {
+
+        //                 sh '''
+        //                     terraform init
+        //                     terraform validate
+        //                     terraform plan
+        //                     terraform apply -auto-approve
+        //                 '''
+        //             }
+        //         }
+        //     }
+        // }
+
+        //  stage('Terraform EKS') {
+        //     steps {
+        //         dir('/home/ec2-user/jenkins-shared-library/jenkins-terraform-eks/90-eks'){
+        //             withCredentials([[
+        //                 $class: 'AmazonWebServicesCredentialsBinding',
+        //                 credentialsId: 'aws-cred'
+        //             ]]) {
+
+        //                 sh '''
+        //                     terraform init
+        //                     terraform validate
+        //                     terraform plan
+        //                     terraform apply -auto-approve
+        //                 '''
+        //             }
+        //         }
+        //     }
+        // }
+
+    }   // <-- Close stages block
+
+    post {
+
+        always {
+            echo "Cleaning Workspace..."
+            cleanWs()
+        }
+
+        success {
+            echo "Pipeline Completed Successfully"
+        }
+
+        failure {
+            echo "Pipeline Failed"
+        }
+
+        aborted {
+            echo "Pipeline Aborted"
+        }
+    }
+
+}
+
+}
